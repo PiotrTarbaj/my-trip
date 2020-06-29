3f9b80d3f0e5c82b12b11d20a9d394279cef2aa9
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-06-30 01:16:10 +0200

    Dodano strukturę komponentu Map

---
+++ src/Map/Map.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Map/Map.scss";
+
+const Map = () => {
+  return (
+    <div className="container-map">
+      <h1>Mapa</h1>
+    </div>
+  );
+};
+
+export default Map;
