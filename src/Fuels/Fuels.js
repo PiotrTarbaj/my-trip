971ccc5fb70957b8e62312bc75bd852731d94d94
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-06-30 01:09:24 +0200

    Dodano strukturę komponentu Fuels

---
+++ src/Fuels/Fuels.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Fuels/Fuels.scss";
+
+const Fuels = () => {
+  return (
+    <div className="container-fuels">
+      <h1>Fuels</h1>
+    </div>
+  );
+};
+
+export default Fuels;
