8930b4cca30bee90874a4ce4bc904b80d9d2c4b4
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-06-30 01:08:46 +0200

    Dodano strukturę komponentu Cars

---
+++ src/Cars/Cars.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Cars/Cars.scss";
+
+const Cars = () => {
+  return (
+    <div className="container-cars">
+      <h1>Cars</h1>
+    </div>
+  );
+};
+
+export default Cars;
