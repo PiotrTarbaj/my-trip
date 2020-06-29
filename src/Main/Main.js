d12173069885f54676e14b18afc31a767a7f976f
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-06-30 01:09:52 +0200

    Dodano strukturę komponentu Main

---
+++ src/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Main/Main.scss";
+
+const Main = () => {
+  return (
+    <div className="container-main">
+      <h1>Main</h1>
+    </div>
+  );
+};
+
+export default Main;
