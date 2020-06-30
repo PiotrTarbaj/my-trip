5f12486ad9e97c147604c49fc5186afc12d326ba
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-07-01 01:43:01 +0200

    Dodano logikę do komponentu Cars

--- src/Cars/Cars.js
+++ src/Cars/Cars.js
@@ -1,11 +1,65 @@
-import React from "react";
-import "../Cars/Cars.scss";
+import React, { useEffect, useState } from "react";
+import Car from "./Car";
+import EditCar from "./EditCar";
+import AddCar from "./AddCar";
+import "../";
 
 const Cars = () => {
+  const [cars, setCars] = useState([]);
+  const [isEdit, setEdit] = useState(false);
+
+  const getDataFromApi = () => {
+    fetch("http://localhost:3000/cars")
+      .then((response) => response.json())
+      .then((data) => {
+        setCars(data);
+      });
+  };
+
+  const handleAdd = (car) => {
+    setCars([...cars, car]);
+  };
+
+  const handleEdit = (car) => {
+    setEdit(car);
+  };
+
+  const handleSave = () => {
+    setEdit(false);
+    getDataFromApi();
+  };
+
+  const deleteSelectedFromAPI = (id) => {
+    fetch(`http://localhost:3000/cars/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        getDataFromApi();
+      });
+  };
+
+  useEffect(() => {
+    getDataFromApi();
+  }, []);
+
   return (
-    <div className="container-cars">
-      <h1>Cars</h1>
-    </div>
+    <>
+      <h1>Car</h1>
+      {isEdit && <EditCar car={isEdit} onSave={handleSave} />}
+      <AddCar onAdd={handleAdd} />
+      <ul>
+        {cars.length > 0 &&
+          cars.map((car, i) => (
+            <Car
+              key={car.id}
+              car={car}
+              onEdit={handleEdit}
+              onDelete={deleteSelectedFromAPI}
+            />
+          ))}
+      </ul>
+    </>
   );
 };
 
