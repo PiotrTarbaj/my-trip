44d22c5026c3f7dcd7455acddce73b9b5b9bb4c1
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-06-30 23:59:37 +0200

    Dodano stylizację dla sekcji

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../Main/Main.scss";
 
 const Main = () => {
   return (
     <div className="container-main">
-      <h1>Main</h1>
+      <section>
+        <Link to="/cars">
+          <h2>Moje samochody</h2>
+        </Link>
+      </section>
+      <section>
+        <Link to="/fuels">
+          <h2>Koszty paliwa</h2>
+        </Link>
+      </section>
+      <section>
+        <Link to="/map">
+          <h2>Wyznacz trasę</h2>
+        </Link>
+      </section>
     </div>
   );
 };
