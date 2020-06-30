e7d6a8bf27de50798bb1f2f979506638dbc5a5e8
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-06-30 21:13:50 +0200

    Dodano treść dla błędu 404

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -5,8 +5,11 @@ import "../NotFound/NotFound.scss";
 const NotFound = () => {
   return (
     <div className="container-notfound">
-      <h1>Błąd 404</h1>
-      <Link to="/">Przejdź do strony głównej</Link>
+      <section>
+        <h1>Błąd 404</h1>
+        <h3>Nie ma takiej strony</h3>
+        <Link to="/">{">>Przejdź do strony głównej<<"}</Link>
+      </section>
     </div>
   );
 };
