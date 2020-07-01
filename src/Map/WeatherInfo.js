8c5d1e889faf4ea8bb3ae150be87e4f6c408bbc6
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-07-02 01:30:35 +0200

    Dodano komponent do wyświetlania biezącej pogody w zadanej miejscowości

---
+++ src/Map/WeatherInfo.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+
+const WeatherInfo = () => {
+  const [isData, setIsData] = useState(false);
+  const [apiResultsWeather, setApiResultsWeather] = useState([]);
+  const [apiResultsMain, setApiResultsMain] = useState([]);
+
+  const apiKey = "562b8ae9683647da716b61115038f714";
+
+  const placeCoordinate = {
+    lat: 51.940267,
+    lng: 15.507312,
+  };
+
+  useEffect(() => {
+    const API = `https://api.openweathermap.org/data/2.5/weather?lang=pl&units=metric&lat=${placeCoordinate.lat}&lon=${placeCoordinate.lng}&appid=${apiKey}`;
+    console.log(API);
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.cod === 200) {
+          setIsData(true);
+          Object.keys(data).map((key) => {
+            if (key === "weather") {
+              setApiResultsWeather(data[key][0]);
+            }
+            if (key === "main") {
+              setApiResultsMain(data[key]);
+            }
+          });
+        } else {
+          setIsData(false);
+        }
+      })
+      .catch((error) => {
+        setIsData(false);
+      });
+  }, []);
+
+  return (
+    <>
+      {isData && (
+        <div>
+          <h1>WeatherInfo</h1>
+          <img
+            src={`http://openweathermap.org/img/wn/${apiResultsWeather.icon}@4x.png`}
+            alt={apiResultsWeather.main}
+          />
+          <h4 style={{ textTransform: "capitalize" }}>
+            {apiResultsWeather.description}
+          </h4>
+          <ul>
+            <li>
+              <strong>Temperatura:</strong> {apiResultsMain.temp}°C
+            </li>
+            <li>
+              <strong>Odczuwalna:</strong> {apiResultsMain.feels_like}°C
+            </li>
+            <li>
+              <strong>Ciśnienie:</strong> {apiResultsMain.pressure} hPa
+            </li>
+            <li>
+              <strong>Wilgotność:</strong> {apiResultsMain.humidity}%
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default WeatherInfo;
