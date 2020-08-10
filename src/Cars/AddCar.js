67cfd4096915284d7498afe27ec6b5a1b3b1b967
Piotr Tarbaj <peterfm@poczta.onet.pl>
2020-08-11 00:28:48 +0200

    Dodano odstęp pomiędzy inputami

--- src/Cars/AddCar.js
+++ src/Cars/AddCar.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Fuels from "../Fuels/Fuels";
 
-const AddCar = ({ onAdd }) => {
+const AddCar = ({ onAdd, onCancel, title }) => {
   const [brand, setBrand] = useState("");
   const [model, setModel] = useState("");
-  const [fuel, setFuel] = useState("");
-  const [average, setAverage] = useState("");
-  const [speed, setSpeed] = useState("");
+  const [fuel, setFuel] = useState("benzyna");
+  const [average, setAverage] = useState(0);
+  const [speed, setSpeed] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +17,6 @@ const AddCar = ({ onAdd }) => {
       average: average,
       speed: speed,
     };
-    console.log(newCar);
     fetch("http://localhost:3000/cars", {
       method: "POST",
       body: JSON.stringify(newCar),
@@ -31,9 +30,19 @@ const AddCar = ({ onAdd }) => {
       });
   };
 
+  const handleCancel = () => {
+    onCancel();
+  };
+
+  useEffect(() => {
+    //const fuelElement = document.querySelector('select[name="fuel"]');
+    //console.log("Paliwo: " + fuelElement.options.length);
+  }, []);
+
   return (
     <div className="container-cars--edit">
       <form onSubmit={handleSubmit}>
+        <h2 className="container-cars--title">{title}</h2>
         <input
           type="text"
           name="brand"
@@ -50,20 +59,25 @@ const AddCar = ({ onAdd }) => {
         />
         <Fuels show="select" value={fuel} onChange={setFuel} />
         <input
-          type="text"
+          type="number"
+          min="0"
+          max="100"
           name="average"
           placeholder="Średnie spalanie"
           value={average}
           onChange={(e) => setAverage(e.target.value)}
         />
         <input
-          type="text"
+          type="number"
+          min="0"
+          max="300"
           name="speed"
           placeholder="Średnia prędkość"
           value={speed}
           onChange={(e) => setSpeed(e.target.value)}
         />
         <input type="submit" value="Dodaj" />
+        <input type="button" value="Anuluj" onClick={() => handleCancel()} />
       </form>
     </div>
   );
